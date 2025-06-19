cbc2be3f1ef88bb1ec3621799aede565cf3af21e
Tomunek <kowalczyk.tomek11@gmail.com>
2025-06-20 01:05:57 +0200

    Edit user started

--- database/sqldb/schema.go
+++ database/sqldb/schema.go
@@ -5,6 +5,7 @@ CREATE TABLE IF NOT EXISTS users (
 	uuid UUID PRIMARY KEY,
 	username TEXT NOT NULL,
 	email TEXT NOT NULL,
+	description TEXT NOT NULL,
 	password BYTEA NOT NULL,
 	role TEXT NOT NULL,
 	deleted TIMESTAMP,
--- database/user.go
+++ database/user.go
@@ -7,14 +7,15 @@ import (
 )
 
 type User struct {
-	UUID      uuid.UUID  `db:"uuid"`
-	Username  string     `db:"username"`
-	Email     string     `db:"email"`
-	Password  []byte     `db:"password"`
-	Role      role       `db:"role"`
-	Deleted   *time.Time `db:"deleted"`
-	Created   time.Time  `db:"created"`
-	LastLogin *time.Time `db:"last_login"`
+	UUID        uuid.UUID  `db:"uuid"`
+	Username    string     `db:"username"`
+	Email       string     `db:"email"`
+	Password    []byte     `db:"password"`
+	Description string     `db:"description"`
+	Role        role       `db:"role"`
+	Deleted     *time.Time `db:"deleted"`
+	Created     time.Time  `db:"created"`
+	LastLogin   *time.Time `db:"last_login"`
 }
 
 type role string
